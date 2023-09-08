a29ac4e292f6e60c816c7fa1b33de724cb2c2ec6
matthieu <m.doutreligne@has-sante.fr>
2023-09-08 09:46:18 +0200

    add confounder sensitivityé

--- reports/6_confounder_sensitivity_albumin_for_sepsis_report.py
+++ reports/6_confounder_sensitivity_albumin_for_sepsis_report.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 from caumim.constants import *
 from caumim.reports_utils import add_albumin_label, add_rct_gold_standard_line
-from caumim.variables.selection import FEATURE_SETS
+from caumim.variables.selection import FEATURE_SETS, LABEL_ALL_FEATURES
 
 # %%
 cohort_name = "sensitivity_confounders_albumin_for_sepsis__bs_50"
@@ -27,9 +27,8 @@ outcome_name = COLNAME_MORTALITY_28D
 mask_no_models = results["estimation_method"].isin(
     ["Difference in mean", LABEL_RCT_GOLD_STANDARD_ATE]
 )
-results = results.loc[~mask_no_models].reset_index(drop=True)
+# results = results.loc[~mask_no_models].reset_index(drop=True)
 
-# %%
 results["estimation_method"] = results["estimation_method"].map(
     lambda x: IDENTIFICATION2LABELS[x]
     if x in IDENTIFICATION2LABELS.keys()
@@ -37,12 +36,10 @@ results["estimation_method"] = results["estimation_method"].map(
 )
 
 results["label"] = (
-    # "Features="
-    # + results["feature_subset"]
     "Est="
     + results["estimation_method"]
     + " + "
-    + +results["treatment_model"].map(
+    + results["treatment_model"].map(
         lambda x: ESTIMATORS2LABELS_SHORT[x]
         if x in ESTIMATORS2LABELS.keys()
         else x
@@ -51,15 +48,22 @@ results["label"] = (
 results.loc[mask_no_models, "label"] = results.loc[
     mask_no_models, "estimation_method"
 ]
-# NO_MODEL_GROUP_LABEL = ""
-# results.loc[mask_no_models, "estimation_method"] = NO_MODEL_GROUP_LABEL
-# results.loc[mask_no_models, "feature_subset"] = NO_MODEL_GROUP_LABEL
+
+NO_MODEL_GROUP_LABEL = ""
+results.loc[mask_no_models, "estimation_method"] = NO_MODEL_GROUP_LABEL
+results.loc[mask_no_models, "feature_subset"] = NO_MODEL_GROUP_LABEL
 results["estimation_method"] = results["estimation_method"].map(
     lambda x: IDENTIFICATION2LABELS[x]
     if x in IDENTIFICATION2LABELS.keys()
     else x
 )
-results["sortby"] = results["feature_subset"] + "_" + results["treatment_model"]
+results["sortby"] = (
+    results["feature_subset"]
+    + "_"
+    + results["treatment_model"]
+    + "_"
+    + results["estimation_method"]
+)
 #
 print(
     results.groupby(["feature_subset", "estimation_method", "treatment_model"])[
@@ -72,19 +76,30 @@ results["ntv"] = results["ntv"].map(lambda x: f"{x:.2f}" if x > 0 else "")
 #     for ident_ in list(IDENTIFICATION2LABELS.values())
 #     if ident_ in results["estimation_method"].unique()
 # ]
+# non understood error for one line:
+mask_all_features = results["feature_subset"] == LABEL_ALL_FEATURES
+results.loc[mask_all_features, "label"] = results.loc[
+    mask_all_features, "label"
+].map(lambda x: x + ".")
+
 results["feature_subset"] = results["feature_subset"].map(
     lambda x: x + f" ({len(FEATURE_SETS[x])} features)"
     if x in FEATURE_SETS.keys()
     else x
 )
 group_order = [
+    NO_MODEL_GROUP_LABEL,
     "All confounders (24 features)",
     "Without drugs (19 features)",
     "Without measurements (13 features)",
     "Socio-demographics (5 features)",
 ]
-# results["feature_subset"].unique()
-# results = results.sort_values(by="sortby").reset_index(drop=True)
+results = results.sort_values(by="sortby").reset_index(drop=True)
+# results["result_ate_clean"] = results.apply(
+#     lambda x: f"{x[RESULT_ATE]:.2f} ({x[RESULT_ATE_LB]:.2f}, {x[RESULT_ATE_UB]:.2f})",
+#     axis=1,
+# )
+
 # %%
 import forestplot as fp
 
@@ -95,7 +110,6 @@ axes = fp.forestplot(
     hl=RESULT_ATE_UB,  # columns containing conf. int. lower and higher limits
     varlabel="label",  # column containing variable label
     xlabel=f"ATE on {OUTCOME2LABELS[outcome_name]}",  # x-label title
-    # annote=["treatment_model", "event_aggregation"],  # columns to annotate
     groupvar="feature_subset",  # group variable
     group_order=group_order,
     rightannote=["ntv"],  # columns to report on right of plot
@@ -105,9 +119,8 @@ axes = fp.forestplot(
     sortby="sortby",
     ylabel="ATE (95% bootstrap confidence interval)",  # ylabel to print
     **{"marker": "D", "ylabel1_size": 10, "ylabel1_fontweight": "normal"},
-    table=False,
 )
-axes = add_albumin_label(axes, x_less=1.18, fontsize=10)
+axes = add_albumin_label(axes, x_less=1.11, fontsize=10)
 
 path2img = DIR2DOCS_IMG / cohort_name
 path2img.mkdir(exist_ok=True, parents=True)
